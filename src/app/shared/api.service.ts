9daa475f455f07caa10f88998b05b78a0e6f4475
Larss <larslarsandersen@gmail.com>
2018-05-13 22:27:37 -0300

    Better error handling on api service

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
-import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
-import { Observable ,  of } from 'rxjs';
+import { Observable, throwError,  of } from 'rxjs';
 
 @Injectable()
 export class ApiService {
@@ -15,14 +15,18 @@ export class ApiService {
   get(url: string): Observable<any> {
     return this.http.get<any>(this.getFullURL(url), this.getHttpOptions())
       .pipe(
-        catchError(this.handleError<any>())
+        catchError(this.handleError)
       );
+
+
   }
 
   post(url: string, body: Object): Observable<any> {
-    return this.http.post<any[]>(this.getFullURL(url), body, this.getHttpOptions())
+
+
+    return this.http.post<any>(this.getFullURL(url), body, this.getHttpOptions())
       .pipe(
-        catchError(this.handleError())
+        catchError(this.handleError)
       );
   }
 
@@ -45,18 +49,26 @@ export class ApiService {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
         'Authorization': `Bearer ${this.authService.getToken()}`
-      })
+      }),
+      observe: 'response'
     };
   }
 
-  private handleError<T> (result?: T) {
-    return (error: any): Observable<T> => {
 
-      console.error(error);
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${JSON.stringify(error.error)}`);
+    }
+
+    return throwError(error);
+  };
 
 
 }
